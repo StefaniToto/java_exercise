874e461faa99f762832da8cf814b671fd1048a0b
stefani7 <stefani.toto@gmail.com>
2019-01-19 22:54:51 +0100

    mvc

---
+++ MVC_example/src/CalcModel.java
@@ -0,0 +1,24 @@
+
+public class CalcModel {
+
+	
+	 // Holds the value of the sum of the numbers
+    // entered in the view
+
+	 private int calculationValue;
+	
+	void addTwoNumbers(int firstNumber, int secondNumber){
+
+		calculationValue = firstNumber + secondNumber;
+  
+		    }
+
+		    public int getCalculationValue(){
+      
+	        return calculationValue;
+		         
+
+		    }
+	
+	
+}
