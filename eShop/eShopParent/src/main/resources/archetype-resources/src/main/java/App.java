9f14a86c380cb9a99bc9b9df2a7ee5b890fe2912
stefani7 <stefani.toto@gmail.com>
2019-03-18 11:51:31 -0400

    commit 2 17.00

---
+++ eShop/eShopParent/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $al.alexiasolutions.eshop.eShopParent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
